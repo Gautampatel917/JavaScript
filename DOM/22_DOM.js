9a66817bf5562e8ac193daf487b593848abd006d
Gautampatel917 <118241295+Gautampatel917@users.noreply.github.com>
2024-06-13 16:20:21 +0530

    Add files via upload

---
+++ DOM/22_DOM.js
@@ -0,0 +1,18 @@
+
+document.getElementById("heading")
+    < h1 class=​"heading" id = "heading" > DOM Learning on Chai or Code​</h1 >
+
+        document.getElementById("heading").innerHTML = "<h1>Success</h1> 
+
+document.getElementById("heading").getAttribute('id')
+
+document.getElementById("heading").setAttribute("class", "test heading")
+
+const heading = document.getElementById('heading')
+
+const tempList = document.querySelectorAll("h2");
+
+const myConvertedArray = Array.from(tempValue)
+
+// ------------------------------------------------------------------------
+
